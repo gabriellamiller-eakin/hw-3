9f51b7b34c3934a9b58efe1e3c7782f1351b45d7
Gabby Miller <gabriellaeakin@gmail.com>
2020-10-07 18:55:18 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # hw-3
+for this assignment, I had the following acceptance criteria: 
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN prompted for character types to include in the password
+THEN I choose lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+I started by creating variables for each of the different confirmations I would ask the user (password preferences they can choose from (true vs. false)): 
+- uppercase 
+- lowercase
+- special characters 
+- numbers 
+
+After that, I created an empty string to use later (to push the randomly selected password into)
+
+Next, I prompted the user to ask how many characters they would like their password to be. If the number they returned was greater than 128 or less than 8, it would return an empty string into the password generator. 
+
+If they selected a valid number of characters, they were asked a series of confirmation statements to go ahead and make their password preference selections. 
+
+For each true statement, I would push the related array into another empty array that was created inside of the function. 
+
+Once all selections were made, I created a loop that was determined by the password length set by the user. 
+
+That loop was set to randomly select from the arrays that were pushed into our empty array, and then taken a step further to randomly select items inside each of the arrays inside of the array. 
+
+Once that was completed, I set my empty string to be += the randomly generated password and returned it to be shown on the screen once the user was through all of the prompts/confirmations. 
+
+I used dev tools to console log my code throughout this process to make sure the code was working as expected. 
--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L",'M","N","O","P"
 var numeric = ["0","1","2","3","4","5","6","7","8","9"];
 var special = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","/", "]","^","_","`","{","|","}","~"]
 
+var finalPass = "";
 
 // Write password to the #password input
 function writePassword() {
@@ -15,47 +16,70 @@ function writePassword() {
 
 }
 
-// function for all of the prompts and user selected criteria 
-generateBtn.addEventListener("click", function (passCriteria) {
+// defining the function for what happens above with generate password 
 
-// storing a variable for the user's selected length "lengthPrompt", and then storing the answer as a parseINT
-var lengthPrompt = prompt("how many characters would you like your password to be? Please choose at least 8 characters, and no more than 128 characters.");
-lengthPrompt = parseInt(lengthPrompt)
+function generatePassword () {
 
-if (lengthPrompt>=8 && lengthPrompt<=128) {
-    console.log(lengthPrompt);
-    console.log(generatedpassnew);
-
+// if the user does not pick a number between 8 & 128, it returns an empty string
+  
+var passLength = prompt("how long do you want your password to be?");
+if (passLength>128 || passLength<8) {
+  return "";
 }
 
-else { 
-    alert("please enter a number between 8 and 128!");
-    return;
+// creating an empty array to push the other arrays into depending on what the user chooses as true vs. false 
+
+var passwordSelections = [];
+
+// confirm items for the user to select what they would like the pass criteria to be 
+
+var charLower = confirm("do you want your password to include lowercase?")
+
+var charUpper = confirm("do you want your password to include uppercase?")
+
+var charNum = confirm("do you want your password to include numbers?")
+
+var charSpec = confirm("do you want your password to include special characters?")
+
+// for true on each of the above statements, this code pushes the arrays defined above into our new empty array 
+if (charLower) {
+  passwordSelections.push(lowercase);
 }
+
+if (charUpper) {
+  passwordSelections.push(uppercase);
 }
 
-// Special characters loop 
+if (charNum) {
+  passwordSelections.push(numeric);
+}
 
-// var confirmSpec = confirm("Would you like your password to contain special characters?")
-//     console.log(confirmSpec);
+if (charSpec) {
+  passwordSelections.push(special);
+}
 
-// if (confirmSpec) {
-//     for (var i=0; i<(special.length); i++) {
-//         var randomSpec = special[Math.floor(Math.random()*special.length)];
-//     }
-//     console.log(randomSpec);
+console.log(passwordSelections);
 
-// }
+// we want this to loop the number of times set by the user, and then to randomly select from the password selections array 
+for (var i=0; i<passLength; i++) {
 
-// var confirmLower = confirm("include lower cases?");
+  var randomSelections = Math.floor(Math.random()*passwordSelections.length);
+  console.log(randomSelections);
+  var randompassgen = passwordSelections[randomSelections];
+  console.log(randompassgen);
+   // when we did this random select, it returned random arrays inside of our array, so now we need to do another random math function to randomly select items within each array to return 
+  var randomfinal = randompassgen[Math.floor(Math.random()*randompassgen.length)]
+  console.log(randomfinal);
 
-// var confirmUpper = confirm("include uppercase?");
+  // now we need to set finalPass to equal the random password we generated above. 
+  finalPass += randomfinal;
+}
 
-// var confirmNum = confirm("inlcude numbers?");
-// }
+// return final pass will put the randomly generated password into our password box in the HTML
 
-// Add event listener to generate button
+return finalPass;
+}
 
-generateBtn.addEventListener("click", writePassword);
+// when the button is clicked, the random password is displayed! 
 
-generateBtn.addEventListener("click", passCriteria); 
+generateBtn.addEventListener("click", writePassword) 
